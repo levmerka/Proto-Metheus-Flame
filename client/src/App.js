f8f735f258bb9a349e18290d66dce3ef87e539bd
Manachey <mezatx@gmail.com>
2022-09-02 13:01:31 -0500

    Updated Login Page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
-
 import './App.css';
 import HomePage from './components/HomePage'
+import StyleSheet from './components/Stylesheet';
 
 function App() {
   return (
     <div className="App">
+      <StyleSheet/>
       <HomePage/>
     </div>
   );
