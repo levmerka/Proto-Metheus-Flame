559569eb0e62846a5d38beb7706cb7c140e4956c
Mitchell Lev Merka <lev@merka.us>
2022-09-05 23:50:42 -0700

    fixed seed.js

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,15 +1,15 @@
 const db = require("../config/connection");
-const { User, File } = require("../models");
+const { User, Track } = require("../models");
 
 const userData = require("./users.json");
 const fileData = require("./files.json");
 
 db.once("open", async () => {
   await User.deleteMany({});
-  await File.deleteMany({});
+  await Track.deleteMany({});
 
   const userSeeds = await User.insertMany(userData);
-  const fileSeeds = await File.insertMany(fileData);
+  const trackSeeds = await Track.insertMany(fileData);
 
   console.log("example files loaded.");
   process.exit(0);
